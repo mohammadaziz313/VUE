cf019f917e3b3655537b9b0493a2881e7e89c767
azizmd313 <azizmd313@gmail.com>
2019-10-23 14:53:08 +0900

    Monster Slayer Game

--- Project 1/Setup/app.js
+++ Project 1/Setup/app.js
@@ -7,30 +7,71 @@ new Vue({
     logger: []
   },
   methods: {
-    changeGameStatus: function(message) {
+    changeGameStatus: function() {
       this.isGameStart = !this.isGameStart;
     },
+    initGame: function() {
+      this.logger = [];
+      this.isGameStart = false;
+      this.userHealth = this.monsterHealth = 100;
+    },
+    endGame: function() {
+      this.logger = [];
+      this.isGameStart = true;
+      this.userHealth = this.monsterHealth = 100;
+    },
     attack: function() {
-      var userRand = generateRandom(1);
-      var monsterRand = generateRandom(1);
+      var userRand = this.generateRandom(1);
+      var monsterRand = this.generateRandom(1);
       this.logger.unshift("Monster Attacked User for " + monsterRand);
       this.logger.unshift("User Attacked Monster for " + userRand);
-      this.userHealth -= userRand;
-      this.monsterHealth -= monsterRand;
+
+      this.checkWinner(userRand, monsterRand);
     },
     specialAttack: function() {
-      var userRand = generateRandom(10);
-      var monsterRand = generateRandom(10);
+      var userRand = this.generateRandom(10);
+      var monsterRand = this.generateRandom(10);
       this.logger.unshift("Monster Special Attacked User for " + monsterRand);
       this.logger.unshift("User Special Attacked Monster for " + userRand);
-      this.userHealth -= userRand;
-      this.monsterHealth -= monsterRand;
+      this.checkWinner(userRand, monsterRand);
+    },
+    checkWinner: function(userRand, monsterRand) {
+      if (this.userHealth - userRand < 0) this.userHealth = 0;
+      else this.userHealth -= userRand;
+
+      if (this.monsterHealth - monsterRand < 0) this.monsterHealth = 0;
+      else this.monsterHealth -= monsterRand;
+
+      if (this.monsterHealth == 0 && this.userHealth == 0) {
+        if (confirm("It is a draw!!\nDo you want to restart?")) this.initGame();
+        else this.endGame();
+      } else if (this.monsterHealth == 0) {
+        if (confirm("You won!!\nDo you want to restart?")) this.initGame();
+        else this.endGame();
+      } else if (this.userHealth == 0) {
+        if (confirm("You lost!!\nDo you want to restart?")) this.initGame();
+        else this.endGame();
+      }
     },
     heal: function() {
-      var userRand = generateRandom(1);
-      var monsterRand = generateRandom(1);
+      var userRand = this.generateRandom(10);
+      var monsterRand = this.generateRandom(1);
+      if (this.userHealth + userRand - monsterRand > 100) {
+        this.userHealth = 100;
+      } else {
+        this.userHealth += userRand - monsterRand;
+        this.logger.unshift("Monster Attacked User for " + monsterRand);
+        this.logger.unshift("User Healed for " + userRand);
+      }
     },
-    giveup: function() {}
+    giveup: function() {
+      if (confirm("Do you really want to quit?")) {
+        this.endGame();
+      }
+    },
+    generateRandom: function(factor) {
+      return Math.floor(Math.random() * 10 + factor);
+    }
   },
   computed: {
     checkGameStatus: function() {
@@ -43,11 +84,8 @@ new Vue({
     },
     getMonsterHealth: function() {
       return {
-        width: this.userHealth + "%"
+        width: this.monsterHealth + "%"
       };
-    },
-    generateRandom: function(factor) {
-      return Math.floor(Math.random() * 10 + factor);
     }
   },
   watch: {}
